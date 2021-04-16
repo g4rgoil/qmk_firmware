4d20fcc0323ddfafcb48c5fa43a1d5d73e14c7bb
Pascal Mehnert <pascalmehnert@posteo.de>
2021-04-16 15:43:57 +0200

    Add config options to disable RGB matrix when idle

--- keyboards/capsunlocked/cu80/v2_iso_rgb/keymaps/gargoil/config.h
+++ keyboards/capsunlocked/cu80/v2_iso_rgb/keymaps/gargoil/config.h
@@ -1,3 +1,8 @@
 #include "config_common.h"
 
+// Disable RGB Matrix after 10 minutes of inactivity
+#define RGB_DISABLE_TIMEOUT 600000
+
+// Disable RGB Matrix when host PC is suspended
+#define RGB_DISABLE_WHEN_USB_SUSPENDED true
 
--- keyboards/capsunlocked/cu80/v2_iso_rgb/keymaps/gargoil/rules.mk
+++ keyboards/capsunlocked/cu80/v2_iso_rgb/keymaps/gargoil/rules.mk
@@ -1,1 +1,2 @@
+# Enable Link-Time-Optimizations to reduce firmware size
 LTO_ENABLE=yes
